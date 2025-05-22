77a4e3a1036b202fb68e2940f748268303248478
MartyshWin <86982485+MartyshWin@users.noreply.github.com>
2025-05-22 17:37:49 +0500

    Create dev_runner.py
    A file that automatically restarts any changes to the code

---
+++ dev_runner.py
@@ -0,0 +1,16 @@
+# A file that automatically restarts any changes to the code
+import asyncio
+import subprocess
+
+from watchfiles import arun_process
+
+def start_bot():
+    # Запуск основного скрипта
+    # python -m main
+    subprocess.run(['python', '-m', 'main'])
+
+async def main():
+    await arun_process('.', target=start_bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
