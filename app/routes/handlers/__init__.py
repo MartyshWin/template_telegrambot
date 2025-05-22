7f0f35bff69b0967a2774bdcda7ab6b557a5097e
MartyshWin <86982485+MartyshWin@users.noreply.github.com>
2025-05-22 17:30:27 +0500

    Create folder handlers and init file
    Create a handlers folder and init file that will connect all handlers files automatically

---
+++ app/routes/handlers/__init__.py
@@ -0,0 +1,25 @@
+import importlib
+import pkgutil
+from aiogram import Router
+from pathlib import Path
+
+main_router = Router()
+
+# Путь к папке handlers
+# Path to the handlers
+package_path = Path(__file__).parent
+package_name = __name__  # "app.handlers"
+
+# Автоматический импорт всех модулей в handlers
+# Automatic import of all modules into handlers
+for _, module_name, is_pkg in pkgutil.iter_modules([str(package_path)]):
+    if is_pkg or module_name.startswith("_"):
+        continue
+
+    module = importlib.import_module(f"{package_name}.{module_name}")
+
+    # Проверяем наличие router
+    if hasattr(module, "router"):
+        router = getattr(module, "router")
+        if isinstance(router, Router):
+            main_router.include_router(router)
